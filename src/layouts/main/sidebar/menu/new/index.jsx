2ed77c13ea02e8c3a3a4ab2ee47cc706eda7aa19
Yusif <yusif.bayramov.yy@gmail.com>
2023-10-02 02:56:18 +0400

    add more category

---
+++ src/layouts/main/sidebar/menu/new/index.jsx
@@ -0,0 +1,10 @@
+import Button from '~/components/button'
+
+function New() {
+  return (
+<div className='py-4 w-[90%]'>
+     <Button size="large">Post</Button>
+     </div>  )
+}
+
+export default New
